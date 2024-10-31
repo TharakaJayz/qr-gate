b9c5124ad4b43c3b16d1ab355823ee62bea90273
TharakaJayz <tharakaprabhath300@gmail.com>
2024-10-31 17:36:17 +0530

    finished navBar bug

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -13,13 +13,12 @@ const NavBar = ({pStyle}: Props) => {
     setMenuLogic((value) => !value)
   }
 
-
   return (
     <div className={`${pStyle} flex justify-between items-center padding-default  bg-primary font-sans `}>
       <h1 className='font-extralight text-xl sm:text-2xl md:text-3xl text-white'>Dashboard</h1>
       <span className='relative'>
         <FaUserCircle className='text-white text-3xl sm:text-[2.5rem] lg:text-[3rem] hover:cursor-pointer' onClick={toggleMenuLogic} />
-        <div className={clsx(`absolute top-[120%] right-0 w-[30vw] h-0 opacity-0  bg-gray-50 border transition-all rounded-lg px-2 py-5 pb-2 flex flex-col items-start justify-end text-gray-500`, menuLogic && "min-h-[10vh] opacity-100")}>
+        <div className={clsx(`absolute top-[120%] right-0 w-[0vw] h-0 opacity-0  bg-gray-50 border transition-all rounded-lg px-2 py-5 pb-2 flex flex-col items-start justify-end text-gray-500 origin-top`, menuLogic && "min-h-[10vh] w-[300px] opacity-100")}>
           Logout
         </div>
       </span>
--- src/components/TopBar.tsx
+++ src/components/TopBar.tsx
@@ -6,20 +6,20 @@ type Props = {
     pStyle?: string
 }
 
-const TopBar = ({pStyle}: Props) => {
+const TopBar = ({ pStyle }: Props) => {
     const navigation = useNavigate();
 
-    const [selectedTitle,setSelectedTitle] = useState<"admin"|"event">("admin");
+    const [selectedTitle, setSelectedTitle] = useState<"admin" | "event">("admin");
 
-   
-  return (
-      <div className={` ${pStyle}  w-full  flex justify-end items-center gap-5 padding-default border-b-2 border-b-gray-300 sm:text-xl`}>
 
-          <span className={clsx(`cursor-pointer`, selectedTitle === "admin" && "font-semibold")} onClick={() => { setSelectedTitle("admin"); navigation("/admin") }}>Admin</span>
-          <span className={clsx(`cursor-pointer`, selectedTitle === "event" && "font-semibold")}onClick={()=>{setSelectedTitle("event"); navigation("/event")}}>Event</span>
+    return (
+        <div className={` ${pStyle}  w-full  flex justify-end items-center gap-5 padding-default border-b-2 border-b-gray-300 sm:text-xl`}>
 
-    </div>
-  )
+            <span className={clsx(`cursor-pointer`, selectedTitle === "admin" && "font-semibold")} onClick={() => { console.log("clicked admin"); setSelectedTitle("admin"); navigation("/admin") }}>Admin</span>
+            <span className={clsx(`cursor-pointer`, selectedTitle === "event" && "font-semibold")} onClick={() => { console.log("clicked event"); setSelectedTitle("event"); navigation("/event") }}>Event</span>
+
+        </div>
+    )
 }
 
 export default TopBar
